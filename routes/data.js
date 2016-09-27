648633f8d6917e4f7be94af5db8c0ee0d3d65662
stormbringer3 <stormbringer3@gmail.com>
2016-09-26 19:31:31 -0600

    globe working on Johns computer

--- routes/data.js
+++ routes/data.js
@@ -18,7 +18,8 @@ function readJSONFile(filename, callback) {
 
 router.get('/', function(req, res, next) {
   console.log('GETTING DATA');
-  readJSONFile('/Users/cameronpacker/Desktop/JavaScript Info/Capstone/gender.json', (err, json) => {
+  // readJSONFile('/Users/cameronpacker/Desktop/JavaScript Info/Capstone/gender.json', (err, json) => {
+  readJSONFile('/Users/stormbringer3/Desktop/DevCap/capstone/gender.json', (err, json) => {
     res.json(json);
   })
 })
