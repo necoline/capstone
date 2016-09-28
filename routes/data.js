cc54bbb39d15463023fb431d0fae4b53b8c32bb9
packercj <packerc@gmail.com>
2016-09-27 20:40:41 -0600

    fixing globe route for gender to populate

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var dir = process.cwd() + '/gender.json';
 
 function readJSONFile(filename, callback) {
   fs.readFile(filename, (err, data) => {
@@ -18,7 +19,7 @@ function readJSONFile(filename, callback) {
 
 router.get('/', function(req, res, next) {
   console.log('GETTING DATA');
-  readJSONFile('/Users/cameronpacker/Desktop/JavaScript Info/Capstone/gender.json', (err, json) => {
+  readJSONFile(dir, (err, json) => {
   // readJSONFile('/Users/***NICOS PATH HERE****capstone/gender.json', (err, json) => {
   // readJSONFile('/Users/stormbringer3/Desktop/DevCap/capstone/gender.json', (err, json) => {
     res.json(json);
