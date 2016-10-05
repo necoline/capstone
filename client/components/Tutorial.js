092c9df8c5fd637d42879eeaa84e34617d05206b
necoline <hnhubner@gmail.com>
2016-10-04 18:53:40 -0600

    sorry guys

---
+++ client/components/Tutorial.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tutorial = () => (
+ <div className="Container">
+  <h1 className="text-align center" style={{ color: 'grey' }}>
+    Learn how to make your own Globe
+  </h1>
+
+   You should only see this if you are logged in.
+ </div>
+);
+
+export default Tutorial;
