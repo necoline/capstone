7d3ecb70ead2f8fff1b3924336c8b821543d9aa3
necoline <hnhubner@gmail.com>
2016-10-09 19:15:16 -0600

    removing broken path from team page

--- client/components/team/Team.js
+++ client/components/team/Team.js
@@ -2,15 +2,14 @@ import React from 'react';
 import JohnBio from './JohnBio';
 import NecoBio from './NecoBio';
 import CameronBio from './CameronBio';
-// import ParkerBio from './ParkerBio';
+import ParkerBio from './ParkerBio';
 import John from '../../../public/images/johncolor.png';
 import Johngs from '../../../public/images/johncolorgs.png';
 import Neco from '../../../public/images/Necolineco.png';
 import Necogs from '../../../public/images/Necolinegs.png';
 import Cameron from '../../../public/images/cameronco.png';
 import Camerongs from '../../../public/images/camerongs.png';
-// import Parker from '../../../public/images/BondParkerBondco.jpg';
-// import Parkergs from '../../../public/images/BondParkerBondgs.png';
+import Parkergs from '../../../public/images/BondParkerBondgs.png';
 
 class Team extends React.Component {
 	constructor(props) {
@@ -53,7 +52,7 @@ class Team extends React.Component {
 					<div className="col m10 offset-m1 fSize">
 						<p>	We are a sincere and dedicated team of students/future graduates of DevPoint Labs.
 								Our mission is to showcase our skills in javascript and provide an easy-to-use presentation tool.
-								It has been an amazing journey to get to where we are today and we are proud to present our project, Globe It! We hope you like what you see and welcome your feedback.
+								It has been an amazing journey to get to where we are today and we are proud to present our project, GlobeIT! We hope you like what you see and welcome your feedback.
 						</p>
 					</div>
 
@@ -113,13 +112,13 @@ class Team extends React.Component {
 							</ul>
 						</div>
 
-						{/* <div className="col m3">
+						<div className="col m3">
 							<a href="#" className="member-profile">
 								<div className="unhover_img">
 								<img src={Parkergs} alt="" />
 								</div>
 								<div className="hover_img" id="parker-click" onClick={this.parkerToggle}>
-								<img src={Parker} alt="BondParkerBond" />
+								<img src={Parkergs} alt="BondParkerBond" />
 								</div>
 								<span className="name-color">Coding Giant(Literally), TA</span>
 								<h4 className="name-color"><span className="first-name">Parker</span><span className="second-name">Bond</span></h4>
@@ -129,7 +128,7 @@ class Team extends React.Component {
 								<li><a href="#"  target="_blank"><i className="fa fa-linkedin"></i></a></li>
 								<li><a href="#" target="_blank"><i className="fa fa-foursquare"></i></a></li>
 							</ul>
-						</div> */}
+						</div>
 						{/* Next row for hidden bios */}
 						<div className="row">
 							<div className="col m3 john-show">
@@ -141,9 +140,9 @@ class Team extends React.Component {
 							<div className="col m3 cameron-show">
 								<CameronBio show={this.state.showCameron} />
 							</div>
-							{/* <div className="col m3 parker-show">
+							<div className="col m3 parker-show">
 								<ParkerBio show={this.state.showParker} />
-							</div> */}
+							</div>
 						</div>
 					</div>
 
