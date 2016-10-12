2ff736b3bde0bae7f06dc91a03e676219d4010d0
necoline <hnhubner@gmail.com>
2016-10-11 18:44:00 -0600

    parker bio fonts

--- client/components/team/ParkerBio.js
+++ client/components/team/ParkerBio.js
@@ -5,7 +5,7 @@ class ParkerBio extends React.Component {
 
 	show() {
 		return (
-			<div style={{ color: 'white' }}>
+			<div className="fSize" style={{ color: 'white' }}>
 				Parker is a 2 meter tall geek who loves programming in React.  He has an affinity for aesthetically pleasing code and insightful improvements.  He loves gaming in all its forms and stick figures by CGP Grey and XKCD.  After an amazing year of teaching at DevPoint Labs, he is moving on to exciting new adventures in coding.
 			</div>
 		);
